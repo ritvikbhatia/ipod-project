c26bf7cbe78aa6ee0332c02d4217355a900c4cf1
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-06-26 02:04:44 +0530

    attach menu

--- src/components/keypad.js
+++ src/components/keypad.js
@@ -4,10 +4,16 @@ import './keypad.css'
 function keypad() {
   return (
     <div className="Keypad">
-      <img id='next' src='https://img.icons8.com/ios-glyphs/2x/fast-forward.png'></img>
-      <img id='back'src='https://img.icons8.com/ios-filled/2x/rewind.png'></img>
-      <img id='play' src='https://img.icons8.com/ios-glyphs/2x/play.png'></img>
+      
+      <img id='next' src='https://img.icons8.com/ios-glyphs/2x/fast-forward.png' alt="next"></img>
+      <img id='back'src='https://img.icons8.com/ios-filled/2x/rewind.png' alt="back"></img>
+      <img id='play' src='https://img.icons8.com/ios-glyphs/2x/play.png' alt="play"></img>
       <span id='menu'>MENU</span>
+      <div id="center"></div>
+     
+
+      
+      
 
     </div>
   );
