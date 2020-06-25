8c57fa31de1e700b97dd16be6661962c22601cad
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-06-25 20:39:04 +0530

    add screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import background from './images/background.png';
+import Screen from './components/screen'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Yo
+      <div className="ipod">
+        <Screen></Screen>
+      </div>
     </div>
   );
 }
+
 export default App;
---
+++ src/components/screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './screen.css'
+function Screen() {
+  return (
+    <div className="Screen">
+        
+    </div>
+  );
+}
+
+export default Screen;
